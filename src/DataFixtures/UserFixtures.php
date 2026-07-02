2556fa8536401947123616dfb1f9b7c217b93068
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-07-03 00:21:54 +0200

    feat: add user fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('jeannemariepredinekraljic@gmail.com');
+        $user->setFirstName('Jeanne-Marie');
+        $user->setLastName('Prédine Kraljic');
+        $user->setRoles(['ROLE_USER']);
+
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            'caca'
+        );
+
+        $user->setPassword($hashedPassword);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
