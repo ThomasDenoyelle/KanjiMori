72169abae112b524061b69541491e9e72777a436
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-07-03 16:12:12 +0200

    feat: add firstname and lastname inputs

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,12 +18,33 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'class' => 'input input-bordered w-full max-w-xs mt-2',
+                ],
+                'row_attr' => [
+                    'class' => 'form-control w-full max-w-xs mb-4'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'class' => 'input input-bordered w-full max-w-xs mt-2',
+                ],
+                'row_attr' => [
+                    'class' => 'form-control w-full max-w-xs mb-4'
+                ]
+            ])
+            ->add('email', null, [
+                'attr' => ['class' => 'input input-bordered w-full max-w-xs mt-2'],
+                'row_attr' => ['class' => 'form-control w-full max-w-xs mb-4']
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue(
-                        message: 'You should agree to our terms.',
+                        message: 'Vous devez accepter les conditions d\'utilisation',
                     ),
                 ],
             ])
@@ -37,7 +59,7 @@ class RegistrationFormType extends AbstractType
                     ),
                     new Length(
                         min: 6,
-                        minMessage: 'Your password should be at least {{ limit }} characters',
+                        minMessage: 'Votre mot de passe doit contenir au minimum 6 caractères',
                         // max length allowed by Symfony for security reasons
                         max: 4096,
                     ),
