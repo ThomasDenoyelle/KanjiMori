4776c814df28466d42c6909ee642dd633c321bcc
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-07-04 01:15:23 +0200

    feat: add question entity

---
+++ src/Entity/Question.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuestionRepository::class)]
+class Question
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $kanji = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $reading = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $translation = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Quiz $quiz = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getKanji(): ?string
+    {
+        return $this->kanji;
+    }
+
+    public function setKanji(string $kanji): static
+    {
+        $this->kanji = $kanji;
+
+        return $this;
+    }
+
+    public function getReading(): ?string
+    {
+        return $this->reading;
+    }
+
+    public function setReading(string $reading): static
+    {
+        $this->reading = $reading;
+
+        return $this;
+    }
+
+    public function getTranslation(): ?string
+    {
+        return $this->translation;
+    }
+
+    public function setTranslation(string $translation): static
+    {
+        $this->translation = $translation;
+
+        return $this;
+    }
+
+    public function getQuiz(): ?Quiz
+    {
+        return $this->quiz;
+    }
+
+    public function setQuiz(?Quiz $quiz): static
+    {
+        $this->quiz = $quiz;
+
+        return $this;
+    }
+}
