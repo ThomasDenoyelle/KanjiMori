0162b34760e38a1cf72964e0fabf0964a4d784a0
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-07-04 19:11:39 +0200

    feat: add cascade persist on question collection

--- src/Entity/Quiz.php
+++ src/Entity/Quiz.php
@@ -32,7 +32,7 @@ class Quiz
     /**
      * @var Collection<int, Question>
      */
-    #[ORM\OneToMany(targetEntity: Question::class, mappedBy: 'quiz', orphanRemoval: true)]
+    #[ORM\OneToMany(targetEntity: Question::class, mappedBy: 'quiz', cascade: ['persist'], orphanRemoval: true)]
     private Collection $questions;
 
     public function __construct()
