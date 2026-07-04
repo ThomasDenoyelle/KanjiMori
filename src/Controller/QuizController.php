c27287b779a9d2127af453892223b57b94168c07
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-07-04 19:22:03 +0200

    feat: add route to create a new quiz

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Quiz;
 use App\Entity\User;
+use App\Form\QuizType;
 use App\Repository\QuizRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted("ROLE_USER")]
+#[IsGranted('ROLE_USER')]
 final class QuizController extends AbstractController
 {
     #[Route('/quiz', name: 'quiz_list')]
@@ -20,6 +24,27 @@ final class QuizController extends AbstractController
 
         return $this->render('quiz/list.html.twig', [
             'quizList' => $quizList,
+        ]);
+    }
+
+    #[Route('/quiz/new', name: 'quiz_new')]
+    public function new(#[CurrentUser] User $user, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $quiz = new Quiz();
+        $quiz->setAuthor($user);
+
+        $form = $this->createForm(QuizType::class, $quiz);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($quiz);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre quiz a bien été créé avec ses questions !');
+            return $this->redirectToRoute('quiz_list');
+        }
+
+        return $this->render('quiz/new.html.twig', [
+            'form' => $form,
         ]);
     }
 }
