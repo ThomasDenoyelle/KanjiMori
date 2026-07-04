8705a13e8c878d0dcc9770797cb82760aa9a254d
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-07-04 20:02:08 +0200

    feat: route to delete a quiz

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -70,6 +70,24 @@ final class QuizController extends AbstractController
         ]);
     }
 
+    #[Route('/quiz/{quiz}/delete', name: 'quiz_delete', methods: ['POST'])]
+    public function delete(#[CurrentUser] User $user, EntityManagerInterface $entityManager, Request $request, ?Quiz $quiz): Response
+    {
+        if (!$quiz || $quiz->getAuthor() !== $user) {
+            $this->addFlash('error', 'Action non autorisée ou quiz introuvable !');
+            return $this->redirectToRoute('quiz_list');
+        }
+
+        if ($this->isCsrfTokenValid('delete' . $quiz->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($quiz);
+            $entityManager->flush();
+            $this->addFlash('success', 'Votre quiz a bien été supprimé');
+        } else {
+            $this->addFlash('error', 'Action non autorisée (Token CSRF invalide).');
+        }
+
+        return $this->redirectToRoute('quiz_list');
+    }
 
 
 }
