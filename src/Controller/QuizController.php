8a55dabf864f6b8fd89ed33f2b1e1d3153745ae5
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-07-04 02:26:09 +0200

    feat: add list route

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\QuizRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted("ROLE_USER")]
 final class QuizController extends AbstractController
 {
-    #[Route('/quiz', name: 'app_quiz')]
-    public function index(): Response
+    #[Route('/quiz', name: 'quiz_list')]
+    public function list(#[CurrentUser] User $user, QuizRepository $quizRepository): Response
     {
-        return $this->render('quiz/index.html.twig', [
-            'controller_name' => 'QuizController',
+        $quizList = $quizRepository->findBy(['author' => $user]);
+
+        return $this->render('quiz/list.html.twig', [
+            'quizList' => $quizList,
         ]);
     }
 }
