17635da8f23f6f86eca4b7a2948d6628b7449f46
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-07-10 17:16:40 +0200

    feat: display in-progress quiz attempts on home page with enhanced layout

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\QuizAttemptRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(#[CurrentUser] User $user, QuizAttemptRepository $quizAttemptRepository): Response
     {
+        $allAttempts = $quizAttemptRepository->findBy(
+            ['author' => $user],
+            ['createdAt' => 'DESC']
+        );
+
+        $inProgressAttempts = array_filter($allAttempts, function($attempt) {
+            $currentAnswers = count($attempt->getAnswerAttempts());
+            return $currentAnswers > 0 && $currentAnswers < $attempt->getMaxScore();
+        });
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'inProgressAttempts' => $inProgressAttempts,
         ]);
     }
 }
