0db9314fa3a3365a0c095a9478a1f882962f70d1
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-07-24 17:56:36 +0200

    feat: add sample quizzes and questions for user fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -77,6 +77,58 @@ class UserFixtures extends Fixture
             $manager->persist($question);
         }
 
+        $prof = new User();
+        $prof->setEmail('professeur@nihongo.com');
+        $prof->setFirstName('Akira');
+        $prof->setLastName('Sensei');
+        $prof->setRoles(['ROLE_USER']);
+        $prof->setIsVerified(true);
+        $prof->setPassword($this->passwordHasher->hashPassword($prof, 'password123'));
+        $manager->persist($prof);
+
+        $quizPublic1 = new Quiz();
+        $quizPublic1->setTitle('Salutations de tous les jours');
+        $quizPublic1->setAuthor($prof);
+        $quizPublic1->setIsPublic(true);
+        $manager->persist($quizPublic1);
+
+        $questionsGreetings = [
+            ['kanji' => '挨拶', 'reading' => 'あいさつ', 'translation' => 'salutations'],
+            ['kanji' => 'おはよう', 'reading' => 'おはよう', 'translation' => 'bonjour (matin)'],
+            ['kanji' => 'こんばんは', 'reading' => 'こんばんは', 'translation' => 'bonsoir'],
+        ];
+
+        foreach ($questionsGreetings as $data) {
+            $question = new Question();
+            $question->setKanji($data['kanji']);
+            $question->setReading($data['reading']);
+            $question->setTranslation($data['translation']);
+            $question->setQuiz($quizPublic1);
+            $manager->persist($question);
+        }
+
+        $quizPublic2 = new Quiz();
+        $quizPublic2->setTitle('Les couleurs (N5)');
+        $quizPublic2->setAuthor($prof);
+        $quizPublic2->setIsPublic(true);
+        $manager->persist($quizPublic2);
+
+        $questionsColors = [
+            ['kanji' => '赤', 'reading' => 'あか', 'translation' => 'rouge'],
+            ['kanji' => '青', 'reading' => 'あお', 'translation' => 'bleu'],
+            ['kanji' => '白', 'reading' => 'しろ', 'translation' => 'blanc'],
+            ['kanji' => '黒', 'reading' => 'くろ', 'translation' => 'noir'],
+        ];
+
+        foreach ($questionsColors as $data) {
+            $question = new Question();
+            $question->setKanji($data['kanji']);
+            $question->setReading($data['reading']);
+            $question->setTranslation($data['translation']);
+            $question->setQuiz($quizPublic2);
+            $manager->persist($question);
+        }
+
         $manager->flush();
     }
 }
