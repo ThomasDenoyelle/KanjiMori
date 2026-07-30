d9aebfb71b24fb2fe65f4d64bd27705b2fa52e66
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-07-30 12:35:55 +0200

    feat: configure API Platform and CORS settings with initial setup files

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
