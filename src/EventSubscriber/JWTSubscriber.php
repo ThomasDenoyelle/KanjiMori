fff6cc1c0dac1afe2693b653c4a503e38113175a
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-08-05 14:30:52 +0200

    feat: add JWTSubscriber to customize JWT payload with user ID

---
+++ src/EventSubscriber/JWTSubscriber.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class JWTSubscriber implements EventSubscriberInterface
+{
+    public function onJWTCreated(JWTCreatedEvent $event): void
+    {
+        $user = $event->getUser();
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $payload = $event->getData();
+
+        $payload['id'] = $user->getId();
+
+        $event->setData($payload);
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'lexik_jwt_authentication.on_jwt_created' => 'onJWTCreated',
+        ];
+    }
+}
